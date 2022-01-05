77065284e5c4ec7f1bf6bec533c428ad35698fa7
ELİF AKAR <93909991+akarelif@users.noreply.github.com>
2022-01-06 00:30:12 +0300

    05/01/2022 düzeltme

--- DAY3_constraints.sql
+++ DAY3_constraints.sql
@@ -1,4 +1,4 @@
-/*======================= CONSTRAINTS - KISITLAMALAR ======================================
+/*============== CONSTRAINTS - KISITLAMALAR ======================================
            
     NOT NULL - Bir Sütunun  NULL içermemesini garanti eder. 
     UNIQUE - Bir sütundaki tüm değerlerin BENZERSİZ olmasını garanti eder.  
@@ -11,7 +11,6 @@
     CHECK - Bir sutundaki tüm verilerin belirlenen özel bir şartı sağlamasını garanti eder. 
     DEFAULT - Herhangi bir değer atanamadığında Başlangıç değerinin atanmasını sağlar.
  ========================================================================================*/
- use sys;
  CREATE TABLE calisanlar
     (
         id CHAR(5) PRIMARY KEY, -- not null+ unique
@@ -19,25 +18,24 @@
         maas int NOT NULL,
         ise_baslama DATE
     );
- 
- 
  INSERT INTO calisanlar VALUES( '10002', 'Mehmet Yılmaz' ,12000, '2018-04-14');
    INSERT INTO calisanlar VALUES('10008', null, 5000, '2018-04-14');
-   INSERT INTO calisanlar VALUES('10011', null, 5500, '2018-04-24'); -- null anlam ifade etmediği icin 2. null unique kısıtlamasını bozmaz kod calısır
+   INSERT INTO calisanlar VALUES('10010', null, 5000, '2018-04-14');-- 2. kez null benzersizlik kisitlamasini bozmaz
     INSERT INTO calisanlar VALUES('10004', 'Veli Han', 5000, '2018-04-14');
     INSERT INTO calisanlar VALUES('10005', 'Mustafa Ali', 5000, '2018-04-14'); 
     INSERT INTO calisanlar VALUES('10006', 'Canan Yaş', NULL, '2019-04-12'); -- maas null olamaz
     INSERT INTO calisanlar VALUES('10003', 'CAN', 5000, '2018-04-14');
-    INSERT INTO calisanlar VALUES('10007', 'CAN', 5000, '2018-04-14'); -- can zaten var ikinci defa yazınca hata verir
-    INSERT INTO calisanlar VALUES('10009', 'cem', '', '2018-04-14'); -- maas int oldugu icin '' olamaz
-    INSERT INTO calisanlar VALUES('', 'osman', 2000, '2018-04-14'); -- id char oldugu icin '' kabul eder
-    INSERT INTO calisanlar VALUES('10002', 'ayse yılmaz', 2000, '2018-04-14'); -- id tekrarlı olmaz unique olmali id CHAR(5) PRIMARY KEY, -- not null+ unique
-     INSERT INTO calisanlar VALUES(null, 'ayse yalmaz', 2500, '2018-04-14'); -- id null olamaz PRIMARY KEY, -- not null
-     
-     select * from calisanlar;
-     
-/*     ------------
--- KISITLAMALAR (ORNEK3 - FOREIGN KEY)
+	INSERT INTO calisanlar VALUES('10007', 'CAN', 5000, '2018-04-14');-- can zaten var
+    INSERT INTO calisanlar VALUES('10009', 'cem', '', '2018-04-14');-- '' bunu int kabul etmez
+    INSERT INTO calisanlar VALUES('', 'osman', 2000, '2018-04-14');-- '' bunu (hiçlik) string kabul eder
+     INSERT INTO calisanlar VALUES( '10002', 'ayse Yılmaz' ,12000, '2018-04-14'); -- id tekrarl olamaz primary key 
+      INSERT INTO calisanlar VALUES( null, 'filiz ' ,12000, '2018-04-14');-- primary key old. null yazamayz
+      
+      select * from calisanlar;
+      
+      
+      /*------------
+-- KISITLAMALAR (  FOREIGN KEY)
 ----------------------------------------------------------------*/
       
     CREATE TABLE adresler
@@ -47,30 +45,31 @@
         cadde VARCHAR(30),
         sehir VARCHAR(15),
         CONSTRAINT id_clsnlr FOREIGN KEY(adres_id) REFERENCES calisanlar(id) -- (solda tablo seçiliyken üstteki info dan ismi kontrol edebilirsin)
-    );-- calısanlar baba taplo adresler child tablo baba da bağlama yapılacak key PRIMARY KEY, -- not null+ unique olmalı
-    -- CONSTRAINT id_clsnlr kısıtlama ismi birden fazla child tablo olursa karısmasın diye isim veriyoruz
-    -- FOREIGN KEY(adres_id) child'in baba ya baglandıgı key'i
-    -- REFERENCES calisanlar(id) baba nın bagalanan key'i 
+    );
+    
     INSERT INTO adresler VALUES('10003','Mutlu Sok', '40.Cad.','IST');
     INSERT INTO adresler VALUES('10003','Can Sok', '50.Cad.','Ankara');
     INSERT INTO adresler VALUES('10002','Ağa Sok', '30.Cad.','Antep');
-    -- parent tabloda olmayan id ile child a ekleme yapamaz 
-    -- FOREIGN KEY'ye null değeri atanabilir. 
+    -- parent tabloda olmayan id ile child a ekleme yapamayiz
+    
+    -- FK'ye null değeri atanabilir. 
     INSERT INTO adresler VALUES(NULL,'Ağa Sok', '30.Cad.','Antep');
-    
-    select* from adresler;
-    -- bagantılı tablolarda child'i silmeden PRENT'İ SİLmez
-    drop table adresler;
-    drop table calisanlar;
-    
-    /*============================== ON DELETE CASCADE =============================
+      
+      select * from adresler;
+      -- bağlantl tablolarda child silinmeden parent silinmez
+      drop table calisanlar;
+      drop table adresler; 
+      
+      
+      /*============================== ON DELETE CASCADE =============================
+
   Her defasında önce child tablodaki verileri silmek yerine ON DELETE CASCADE
   silme özelliğini aktif hale getirebiliriz.
   
   Bunun için FK olan satırın en sonuna ON DELETE CASCADE komutunu yazmak yeterli
   
 ==============================================================================*/ 
-CREATE TABLE talebeler
+   CREATE TABLE talebeler
     (
         id CHAR(3) primary key,  
         isim VARCHAR(50),
@@ -80,13 +79,12 @@ CREATE TABLE talebeler
     
     INSERT INTO talebeler VALUES(123, 'Ali Can', 'Hasan',75);
     INSERT INTO talebeler VALUES(124, 'Merve Gul', 'Ayse',85);
-    INSERT INTO talebeler VALUES(125, 'Kemal Yasa', 'Hasan',85);
+	INSERT INTO talebeler VALUES(125, 'Kemal Yasa', 'Hasan',85);
     INSERT INTO talebeler VALUES(126, 'Nesibe Yılmaz', 'Ayse',95);
-    INSERT INTO talebeler VALUES(127, 'Mustafa Bak', 'Can',99);
-    
+	INSERT INTO talebeler VALUES(127, 'Mustafa Bak', 'Can',99);
+
      CREATE TABLE notlar 
-    ( 
-        talebe_id char(3), 
+    ( talebe_id char(3), 
         ders_adi varchar(30), 
         yazili_notu int, 
         CONSTRAINT notlar_fk FOREIGN KEY (talebe_id) 
@@ -95,17 +93,14 @@ CREATE TABLE talebeler
        -- siler.  
        -- mesela bir hastane silindi o hastanedeki bütün kayıtlar silinmeli, oda böyle olur.
        -- cascade yoksa önce child temizlenir sonra parent
-       
-	INSERT INTO notlar VALUES ('123','kimya',75);
+    
+
+    INSERT INTO notlar VALUES ('123','kimya',75);
     INSERT INTO notlar VALUES ('124', 'fizik',65);
     INSERT INTO notlar VALUES ('125', 'tarih',90);
-    INSERT INTO notlar VALUES ('126', 'Matematik',90);
-    
-      -- FK ile birleştirilen tablolardaki tüm verileri çekmek için 
-    -- JOIN İşlemi yapmak gerekir. Bu konuyu sonra göreceğiz.
-	delete from notlar where talebe_id='124';-- child
-	delete from talebeler where id = 124;-- parent
-    
-    select * from talebeler;
-    select * from notlar;
-    
+	INSERT INTO notlar VALUES ('126', 'Matematik',90);   
+      
+      DELETE FROM talebeler
+    WHERE id = 124;
+      
+      
